785d959e770ef16151f2c12a323d306e47bed9f0
ethan6938 <en.hurwitz@gmail.com>
2025-07-24 13:55:15 +0200

    chapter2

---
+++ Chapter_02/chapter2.sql
@@ -0,0 +1,84 @@
+SELECT * FROM teachers;
+
+SELECT first_name FROM teachers;
+
+SELECT last_name, first_name FROM teachers;
+
+SELECT DISTINCT school
+FROM teachers;
+
+SELECT DISTINCT salary
+FROM teachers;
+
+
+SELECT first_name, last_name, school
+FROM teachers
+ORDER BY school DESC;
+
+DELETE FROM teachers
+WHERE first_name IS NULL
+  AND last_name IS NULL
+  AND school IS NULL;
+
+ALTER TABLE teachers ADD COLUMN age INTEGER;
+
+UPDATE teachers SET age = 17 WHERE id = 7;
+UPDATE teachers SET age = 9 WHERE id = 8;
+
+
+SELECT last_name, school, hire_date
+FROM teachers
+WHERE school = 'Myers Middle School';
+
+
+SELECT first_name, last_name, school
+FROM teachers
+WHERE first_name = 'Janet';
+
+
+SELECT first_name, last_name, school, salary
+FROM teachers
+WHERE salary BETWEEN 40000 AND 65000;
+
+
+SELECT first_name
+FROM teachers
+WHERE first_name ILIKE 'sam%';
+
+SELECT first_name
+FROM teachers
+WHERE first_name LIKE 'sam%';
+
+SELECT *
+FROM teachers
+WHERE school = 'Myers Middle School'
+      AND salary < 40000;
+
+
+SELECT *
+FROM teachers
+WHERE last_name = 'Cole'
+      OR last_name = 'Bush';
+
+
+SELECT *
+FROM teachers
+WHERE school = 'F.D. Roosevelt HS'
+      AND (salary < 38000 OR salary > 40000);
+
+
+SELECT first_name, last_name, school, hire_date, salary, age
+FROM teachers
+WHERE school LIKE '%Roos%'
+ORDER BY hire_date DESC;
+
+
+
+CREATE TABLE  (
+    id bigserial,
+    first_name varchar(25),
+    last_name varchar(50),
+    school varchar(50),
+    hire_date date,
+    salary numeric
+);
